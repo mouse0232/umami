f2d31b38adf4e92778013acb96504b67d9ab23fc
mouse0232 <233939513@qq.com>
2024-11-13 11:26:16 +0800

    Delete uptime.config.ts

--- uptime.config.ts
+++
@@ -1,8 +0,0 @@
-const pageConfig = {
-  title: "mouse0232's Status Page",
-  links: [
-    { link: 'https://n8n-vmcq.onrender.com/', label: 'n8n' },
- //   { link: 'https://blog.lyc8503.site/', label: 'Blog' },
- //   { link: 'mailto:me@lyc8503.site', label: 'Email Me', highlight: true },
-  ],
-}
